603b81e14a0cfc4c856fa1fef0cc746471f58aa5
asravani12 <57284298+asravani12@users.noreply.github.com>
2019-11-06 12:11:37 +0530

    firstcommmit

--- loginform.js
+++ loginform.js
@@ -7,10 +7,7 @@ class App extends React.Component
         super(props)
         this.state={
             username='',
-            password='',
-            email='',
-            phonenumber='',
-            address=''
+            password=''
         }
         render()
         {
